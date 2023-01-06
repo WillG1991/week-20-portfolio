e1fa66d4fd30e72a764a4f1f5bb9a34293de7549
William Gehrke <williamegehrke@gmail.com>
2023-01-06 14:12:19 -0500

    styling adjustments phone size not right

--- src/components/GehrkeApiaries/index.js
+++ src/components/GehrkeApiaries/index.js
@@ -5,21 +5,20 @@ function Apiary() {
   return (
     <section className="my-5">
       <h2 id="about" className="text-center varela-font">
-      Gehrke Apiaries
+        Gehrke Apiaries
       </h2>
-
       <div className="my-2 borderBoxPort">
         <div className="container">
           <div className="row">
             <div className="col">
-            <a target="_blank" href="http://www.gehrkeapiaries.com" rel="noreferrer">
-              <img className="selfie" src={GehrkeApiariesImage} alt="apiary site logo" />
+              <a target="_blank" href="http://www.gehrkeapiaries.com" rel="noreferrer">
+                <img className="selfie" src={GehrkeApiariesImage} alt="apiary site logo" />
               </a>
               <h5 className="textSmall">Click the image to be taken to the site!</h5>
             </div>
             <div className="col">
               <h6>
-React built website using custom CSS styling. Minor React MUI components.               </h6>
+                React built website using custom CSS styling. Minor React MUI components.               </h6>
             </div>
           </div>
         </div>
