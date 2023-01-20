b95157e8fc0d5a4840982601f7aab5405a429401
William Gehrke <williamegehrke@gmail.com>
2023-01-20 17:15:34 -0500

    changed index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/style.css';
 import 'semantic-ui-css/semantic.min.css';
+import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-<React.StrictMode>
+const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
 
-<App />
+render(
 
-</React.StrictMode>
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+  
+      <App />
+  
+    </React.StrictMode>,
+  
+    root
+  
+  );
+  
