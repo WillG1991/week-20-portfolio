c460696fe2a6fd6e9826a0ce10faab9c33588edb
William Gehrke <williamegehrke@gmail.com>
2023-01-20 17:10:14 -0500

    deploy plz

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/:home" element={<Home />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
     </Router>
   );
