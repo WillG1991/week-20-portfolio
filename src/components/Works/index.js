d65a65a186a77a7b91ac6a727e2d1076f2db500c
William Gehrke <williamegehrke@gmail.com>
2023-01-22 14:48:32 -0500

    href for works

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -22,23 +22,23 @@ function Works() {
 
 <div class="grid-block">
   <div class="tile">
-    <a class="tile-link" href="#">
-      <img class="tile-img tile-img1" src={apiarySite} alt="Image"/>
+    <a class="tile-link" href="https://ohdangbro.github.io/Streaming-Search/">
+      <img class="tile-img tile-img1" src={searchSite} alt="Image"/>
     </a>
   </div>
 </div>
 
 <div class="grid-block">
   <div class="tile">
-    <a class="tile-link" href="#">
-      <img class="tile-img tile-img2" src={searchSite} alt="Image"/>
+    <a class="tile-link" href="https://github.com/rrvallar/Project-3">
+      <img class="tile-img tile-img2" src={movieSite} alt="Image"/>
     </a>
   </div>
 </div>
 
 <div class="grid-block">
   <div class="tile">
-    <a class="tile-link" href="#">
+    <a class="tile-link" href="http://www.gehrkeapiaries.com">
       <img class="tile-img tile-img3" src={apiarySite} alt="Image"/>
     </a>
   </div>
@@ -46,16 +46,16 @@ function Works() {
 
 <div class="grid-block">
   <div class="tile">
-    <a class="tile-link" href="#">
-      <img class="tile-img tile-img4" src={apiarySite} alt="Image"/>
+    <a class="tile-link" href="https://codeabreak.herokuapp.com/">
+      <img class="tile-img tile-img4" src={codeSite} alt="Image"/>
     </a>
   </div>
 </div>
 
 <div class="grid-block">
   <div class="tile">
-    <a class="tile-link" href="#">
-      <img class="tile-img tile-img5" src={apiarySite} alt="Image"/>
+    <a class="tile-link" href="http://www.christinaserafina.com">
+      <img class="tile-img tile-img5" src={reikiSite} alt="Image"/>
     </a>
   </div>
 </div>
