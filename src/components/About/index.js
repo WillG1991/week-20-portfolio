aab9e3859c80604bba98ccb20aa38c25d7ad068a
William Gehrke <williamegehrke@gmail.com>
2023-01-22 14:40:36 -0500

    media sizing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,8 +16,8 @@ function About() {
     <h3 className="damionFont"><span className="aboutKnow damionFont">KNOW</span> THE DEVELOPER</h3>
     </Fade>
                 <div class="aboutText">I'm a New Jersey native, Bee Keeper, Chicken Dad, Landscaper, Nature Enthuisiast. Leaning in to these hobbies has ultimately inspired my career path of capturing everything through a story-telling lens.  </div>
-        <div class="aboutText"> I believe collaboration and connection are keys to success.. I inspire to capture the beauty in everything through a story-telling lens.   </div>
-        <div class="aboutText">Coming from a human services based background, I believe community and connection are the keys to success.bI look forward to collaborating with you to create the platform to tell your story.</div>
+        <div class="aboutText">Coming from a human services based background, I believe community and connection are the keys to success.   </div>
+        <div class="aboutText">I look forward to collaborating with you to create the platform to tell your story.</div>
       </div>
       <Fade bottom duration={2300}>
       < MeImage className="meLooking"/>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,15 +6,30 @@ import React from "react";
 function Nav() {
   return (
     <>
-      <nav>
-        <div className="navContainer">
-        <li><a href="#">-Home</a></li>
-        <li><a href="#works">-Works</a></li>
-        <li><a href="#about">-About</a></li>
-        <li><a href="#skills">-Skills</a></li>
-        <li><a href="#">-Contact</a></li>
-        </div>
-        </nav>
+   <nav class="navbar navbar-expand-lg bg-black">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">WILL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#hero">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#works">Work</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#skills">Skills</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
       </>
   );
 }
