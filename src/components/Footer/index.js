5f6710fa6c7169dda72a2ddb65d4f114192ba7a4
OhDangBro <103394466+OhDangBro@users.noreply.github.com>
2023-02-19 20:43:31 -0500

    changed footer, added email contact to nav

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,41 @@
 import React from "react";
-import Fade from 'react-reveal/Fade';
-import meLooking from "../../assets/img/meLooking.jpg"
-import MeImage from "../MeImageChange";
+import { LinkedIn } from '@material-ui/icons';
+import { Email } from '@material-ui/icons';
+import { Folder } from '@material-ui/icons';
+
+
 
 
 
 
 function Footer() {
   return (
-    <section className="footer">
-    <hr className="footer-seperator" />
-    <section className="footer-social-media">
-      <a href="https://www.linkedin.com/in/william-gehrke-1a33591a3/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-    </section>
-  </section>
+<section className="footer" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderTop: '1px solid white', paddingTop: '20px', paddingBottom: '20px' }}>
+<section style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  <a href="https://www.linkedin.com/in/william-gehrke-1a33591a3/" target="_blank" rel="noopener noreferrer">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <LinkedIn style={{ fontSize: 30 }} />
+      <span>LinkedIn</span>
+    </div>
+  </a>
+</section>
+<section style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  <a href="mailto:williamgehrke@gmail.com" target="_blank" rel="noopener noreferrer">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Email style={{ fontSize: 30 }} />
+      <span>Contact Me</span>
+    </div>
+  </a>
+</section>
+<section style={{ padding: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  <a href="https://docs.google.com/document/d/17J-rIdyp_p1w4Yhi-fheD4L8HtL0Ux-I/edit?usp=sharing&ouid=105742650677579971027&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Folder style={{ fontSize: 30 }} />
+      <span>See My Resume</span>
+    </div>
+  </a>
+</section>
+</section>
 
 
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,14 +13,11 @@ function Navbar({ handleClick }) {
       {isOpen && (
         <div className="menu-container">
           <div className="menu">
-            <a href="#hero" className="menu-item" onClick={() =>{ setIsOpen(false); handleClick("Home")}} >Home</a>
-            <a href="#works" className="menu-item" onClick={() => { setIsOpen(false); handleClick("Home")}}>Works</a>
-            <a href="#about" className="menu-item" onClick={() => { setIsOpen(false); handleClick("Home")}}>About</a>
-            <a href="#skills" className="menu-item" onClick={() => { setIsOpen(false); handleClick("Home")}}>Skills</a>
-            <a href="#contact" className="menu-item" onClick={() => { setIsOpen(false); handleClick("Contact")}}>Contact</a>
-
-
-           
+            <a href="#hero" className="menu-item" onClick={() => setIsOpen(false)}>Home</a>
+            <a href="#works" className="menu-item" onClick={() => setIsOpen(false)}>Works</a>
+            <a href="#about" className="menu-item" onClick={() => setIsOpen(false)}>About</a>
+            <a href="#skills" className="menu-item" onClick={() => setIsOpen(false)}>Skills</a>
+            <a href="mailto:WilliamEGehrke@gmail.com" className="menu-item" onClick={() => setIsOpen(false)}>Contact</a>
           </div>
         </div>
       )}
